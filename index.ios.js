2a71e9871897ca4e9ab245fa0c02e7f4c543c60b
Steve Quatrale <steveQ@steveQ-rMBP.local>
2015-06-11 18:45:43 -0400

    I added shit.

--- index.ios.js
+++ index.ios.js
@@ -5,55 +5,66 @@
 'use strict';
 
 var React = require('react-native');
+var AddTask = require('./AddTask');
+var TaskList = require('./TaskList');
+
+
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  TabBarIOS,
+  Component
+
 } = React;
 
+class TodoList extends Component {
 
-
-var TodoList = React.createClass({
-  render: function() {
-
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Narwhals yo.
-        </Text>
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'tasklist'
+    };
   }
 
+  render() {
+
+    return(
+
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'tasklist'}
+          icon={{uri: 'tasklist'}}
+          onPress= { () => {
+            this.setState({
+              selectedTab: 'tasklist'
+            });
+          }}>
+
+            <TaskList/>
+
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          selected={this.state.selectedTab === 'addtask'}
+          icon={{uri: 'addtask'}}
+          onPress= { () => {
+            this.setState({
+              selectedTab: 'addtask'
+            });
+          }}>
+
+          <AddTask/>  
+
+        </TabBarIOS.Item>
+
+      </TabBarIOS>
+
+    );
+
+  }
+
+}
 
 
-});
-
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TodoList', () => TodoList);
